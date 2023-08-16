f32dcf8ef406f54f312c6e90f0b5d2dbd7293fc1
RAJU MUTYALA <raju.balu46143@gmail.com>
2023-08-16 19:49:21 +0000

    DAY # SPRING BOOT

---
+++ banking-service/src/main/java/com/bank/service/BankingServiceApplication.java
@@ -0,0 +1,12 @@
+package main.java.com.bank.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingServiceApplication.class, args);
+    }
+}
+
